c479f494ff92ea78972809711ed05c1b19d4daa0
Akash J <95525571+akashjatl@users.noreply.github.com>
2022-11-06 01:26:06 +0530

    picture

--- src/Signup.js
+++ src/Signup.js
@@ -7,6 +7,7 @@ const Signup = () => {
     const[password,setpassword]=useState('')
     const[reenterpassword,setreenter]=useState('')
     const[errmsg,seterrmsg]=useState('')
+    const[img,setimg]=useState('')
     const errref=useRef(0)
     const start=useRef()
     function post(){
@@ -14,7 +15,8 @@ const Signup = () => {
            firstname:{firstname},
            lastname:{lastname},
            username:{email},
-            password: {password}
+            password: {password},
+            img:{img}
           })
           .then(function (response) {
             console.log(response);
@@ -25,7 +27,9 @@ const Signup = () => {
             seterrmsg('Login failed')
           });
     }
-    
+    const handleimage=(e)=>{
+        setimg(URL.createObjectURL(e.target.files[0]));
+    }
     const handlesubmit=(e)=>{
         e.preventDefault()
         console.log('i am in');
@@ -46,15 +50,16 @@ const Signup = () => {
         <form onSubmit={handlesubmit}>
         {errmsg && <p>{errmsg}</p> }
         <label>Firstname</label>
-        <input type={'text'} onChange={(e)=>setfisrtname(e.target.value)} ref={start}></input>
+        <input type={'text'} onChange={(e)=>setfisrtname(e.target.value)} ref={start} required></input>
         <label>Last name</label>
         <input type={'text'}  onChange={(e)=>setlastname(e.target.value)}></input>
         <label>Email Address</label>
-        <input type={'email'}  onChange={(e)=>setemail(e.target.value)}></input>
+        <input type={'email'}  onChange={(e)=>setemail(e.target.value)} required></input>
         <label>Password</label>
-        <input type={'password'}  onChange={(e)=>setpassword(e.target.value)}></input>
+        <input type={'password'}  onChange={(e)=>setpassword(e.target.value)} required></input>
         <label>Re-enter password</label>
-        <input type={'password'}  onChange={(e)=>setreenter(e.target.value)} ref={errref}></input>
+        <input type={'password'}  onChange={(e)=>setreenter(e.target.value)} ref={errref} required></input>
+        <input type={'file'} img src={img} onChange={handleimage}></input>
         <button>Signup</button>
         </form>
     </div>
