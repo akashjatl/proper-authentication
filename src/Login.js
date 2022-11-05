d0b5d38bc3864f7bc12f07046aeaf04b530a1688
Akash J <95525571+akashjatl@users.noreply.github.com>
2022-11-06 00:12:10 +0530

    login field

--- src/Login.js
+++ src/Login.js
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {useState} from 'react'
+import axios from 'axios'
 const Login = () => {
     const [username,setusername]=useState('')
     const [password,setpassword]=useState('')
+    const [errmsg,seterrormsg]=useState('')
+    const handlesubmit =(e)=>{
+        e.preventDefault()
+         axios.post('/user', {
+            username: {username},
+            password: {password}
+          })
+          .then(function (response) {
+            console.log(response);
+            window.localStorage.clear()
+            window.localStorage.setItem('token',response.data)
+          })
+          .catch(function (error) {
+            console.log(error);
+            seterrormsg('Login failed')
+          });
+    }
+    useEffect(()=>{
+        if(window.localStorage.getItem('token')!==null)
+        window.location.replace('http://localhost:3000/Dashboard')
+    },[])
   return (
-    <div>
-        <input type={'email'}></input>
+    <div onSubmit={handlesubmit}>
+        {errmsg && <p>{errmsg}</p>}
+        <input type={'email'} onChange={(e)=>setusername(e.target.value)}></input>
         <br></br>
-        <input type={'password'}></input><br></br>
+        <input type={'password'} onChange={(e)=>setpassword(e.target.value)}></input><br></br>
         <button>Login</button>
     </div>
   )
