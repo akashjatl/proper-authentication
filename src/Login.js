436c1e9089d6d439ffef955860c0e999d889b466
Sanjay <sanjaysj6282@gmail.com>
2022-11-07 22:19:30 +0530

    modified login

--- src/Login.js
+++ src/Login.js
@@ -1,48 +1,70 @@
-import React, { useEffect } from 'react'
-import {useState} from 'react'
-import axios from 'axios'
-const Login = () => {
-    const [username,setusername]=useState('')
-    const [password,setpassword]=useState('')
-    const [errmsg,seterrormsg]=useState('')
-    const handlesubmit = async(e)=>{
-        e.preventDefault()
-        await axios.post('http://127.0.0.1:8000/login/', {
+import React, { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
 
-            username: {username},
-            password: {password}, headers:{
-              'Content-Type': 'application/json',
-              'X-CSRFTOKEN': localStorage.getItem('token')
-     // Authorization: `token ${localStorage.getItem('token')}`
+function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+      const cookies = document.cookie.split(';');
+      for (let i = 0; i < cookies.length; i++) {
+          const cookie = cookies[i].trim();
+          // Does this cookie string begin with the name we want?
+          if (cookie.substring(0, name.length + 1) === (name + '=')) {
+              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+              break;
           }
-            
-          }
-         
-          )
-          .then(function (response) {
-            console.log(response);
-            window.localStorage.clear()
-            window.localStorage.setItem('token',response.data)
-            window.location.replace('http://localhost:3000/Dashboard')
-          })
-          .catch(function (error) {
-            console.log(error);
-            seterrormsg('Login failed')
-          });
-    }
-    useEffect(()=>{
-        if(window.localStorage.getItem('token')!==null)
-        window.location.replace('http://localhost:3000/Dashboard')
-    },[])
-  return (
-    <div onSubmit={handlesubmit}>
-        {errmsg && <p>{errmsg}</p>}
-        <input type={'email'} onChange={(e)=>setusername(e.target.value)} required></input>
-        <br></br>
-        <input type={'password'} onChange={(e)=>setpassword(e.target.value)} required></input><br></br>
-        <button>Login</button>
-    </div>
-  )
+      }
+  }
+  return cookieValue;
 }
+const csrftoken = getCookie('csrftoken');
+const Login = () => {
+  const [username, setusername] = useState("");
+  const [password, setpassword] = useState("");
+  const [errmsg, seterrormsg] = useState("");
+  const handlesubmit = async (e) => {
+    e.preventDefault();
+    await axios
+      .post("http://127.0.0.1:8000/account/login/", {
+        username: { username },
+        password: { password },
+        headers: {
+          "Content-Type": "application/json",
+          // 'X-CSRFTOKEN': csrftoken
+          // Authorization: `token ${localStorage.getItem('token')}`
+        },
+      })
+      .then(function (response) {
+        console.log(response);
+        window.localStorage.clear();
+        window.localStorage.setItem("token", response.data);
+        window.location.replace("http://localhost:3000/Dashboard");
+      })
+      .catch(function (error) {
+        console.log(error);
+        seterrormsg("Login failed");
+      });
+  };
+  useEffect(() => {
+    if (window.localStorage.getItem("token") !== null)
+      window.location.replace("http://localhost:3000/Dashboard");
+  }, []);
+  return (
+    <form onSubmit={handlesubmit}>
+      {errmsg && <p>{errmsg}</p>}
+      <input
+        onChange={(e) => setusername(e.target.value)}
+        required
+      ></input>
+      <br></br>
+      <input
+        onChange={(e) => setpassword(e.target.value)}
+        required
+      ></input>
+      <br></br>
+      <button>Login</button>
+    </form>
+  );
+};
 
-export default Login
+export default Login;
